d981ac6fbdf2664af0791ca346574dd08aae312a
Aditya Arun Ghorpade <118067181+aditya-2911@users.noreply.github.com>
2026-06-23 20:17:01 +0530

    Time: 3 ms (10.39%), Space: 19.8 MB (21.73%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortColors(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def quick_sort(arr):
+            if len(arr) <= 1:
+                return arr
+            pivot = arr[0]
+            left = [x for x in arr[1:] if x < pivot]
+            right = [x for x in arr[1:] if x >= pivot]
+            return quick_sort(left) + [pivot] + quick_sort(right)
+
+        arr[:]= quick_sort(arr)
