279a8114139278a59ba2d2e739009809310f6d2c
Aditya Arun Ghorpade <118067181+aditya-2911@users.noreply.github.com>
2026-06-23 15:40:44 +0530

    Time: 4 ms (98.49%), Space: 19.4 MB (46.86%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -2,28 +2,45 @@ class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         size = len(nums)
         nums.sort()
-        max_diff = float('inf')
+        result = nums[0] + nums[1] + nums[2]
+        min_diff = float('inf')
 
 
         for i in range(0, size - 2):
+            
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
+            
+            minimum_sum = nums[i] + nums[i+1] + nums[i+2]
+            if minimum_sum > target:
+                if abs(minimum_sum - target) < min_diff:
+                    result = minimum_sum
+                break
+                
+            maximum_sum = nums[i] + nums[-1] + nums[-2]
+            if maximum_sum < target:
+                if abs(maximum_sum - target) < min_diff:
+                    min_diff = abs(maximum_sum - target)
+                    result = maximum_sum
+                continue
             
             leftPtr, rightPtr = i + 1, size - 1
             while leftPtr < rightPtr:
                 currentSum = nums[leftPtr] + nums[rightPtr] + nums[i]
-                diff = abs(currentSum - target)
-                if diff < max_diff:
-                    max_diff=diff
-                    result= currentSum
-                
                 if currentSum == target:
-                    return result
+                    return currentSum
 
+                
                 elif currentSum < target:
-                    
+                    diff = abs(currentSum - target)
+                    if diff < min_diff:
+                        min_diff=diff
+                        result= currentSum
                     leftPtr += 1
                 else:
+                    diff = abs(currentSum - target)
+                    if diff < min_diff:
+                        min_diff = diff
+                        result = currentSum
                     rightPtr -= 1
         return result
