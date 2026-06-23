429a37fd1490597bab24df8e62c38a04bafa695b
Aditya Arun Ghorpade <118067181+aditya-2911@users.noreply.github.com>
2026-06-23 11:49:15 +0530

    Time: 19 ms (10.03%), Space: 21 MB (51.07%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,35 +1,15 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        positiveNums = []
-
-        negativeNums = []
         squaredList = []
-        for num in nums:
-            if num < 0:
-                negativeNums.append(num**2)
+    
+        leftPtr, rightPtr=0,len(nums)-1
+        
+        while leftPtr<=rightPtr:
+            if nums[leftPtr]**2 >= nums[rightPtr]**2:
+                squaredList.append(nums[leftPtr]**2)
+                leftPtr+=1
             else:
-                positiveNums.append(num**2)
-        negativeNums.reverse()
-        if len(positiveNums) == 0:
-            return negativeNums
-
-        elif len(negativeNums) == 0:
-            return positiveNums
-
-        else:
-            positivePtr, negativePtr = 0, 0
-            while positivePtr < len(positiveNums) and negativePtr < len(negativeNums):
-                if positiveNums[positivePtr] <= negativeNums[negativePtr]:
-                    squaredList.append(positiveNums[positivePtr])
-                    positivePtr += 1
-                else:
-                    squaredList.append(negativeNums[negativePtr])
-                    negativePtr += 1
-
-            if positivePtr < len(positiveNums):
-                squaredList.extend(positiveNums[positivePtr:])
-
-            elif negativePtr < len(negativeNums):
-                squaredList.extend(negativeNums[negativePtr:])
-
-            return squaredList
+                squaredList.append(nums[rightPtr]**2)
+                rightPtr-=1
+        squaredList.reverse()
+        return squaredList
