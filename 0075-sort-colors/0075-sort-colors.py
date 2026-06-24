3945574083c35d7f48e00ee02e3f6f8690cf24bb
Aditya Arun Ghorpade <118067181+aditya-2911@users.noreply.github.com>
2026-06-24 11:15:37 +0530

    Time: 0 ms (100%), Space: 19.3 MB (60.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,17 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero, one, two=0,0,0
+        size=len(nums)
+        leftPtr,mid,rightPtr=0,0,size-1
 
-        for num in nums:
-            if num==0: zero+=1
-            elif num==1: one+=1
-            else: two+=1
+        while mid<=rightPtr:
+            if nums[mid]==2:
+                nums[mid],nums[rightPtr]=nums[rightPtr],nums[mid]
+                rightPtr-=1
+            
+            elif nums[mid]==0:
+                nums[leftPtr],nums[mid]=nums[mid],nums[leftPtr]
+                leftPtr+=1
+                mid+=1 
+            
+            else:
+                mid+=1
+            
         
-        for i in range(0,len(nums)):
-            if i<zero:
-                nums[i]=0
-            elif i>=zero and i<zero+one:
-                nums[i]=1
-            else: nums[i]=2
+                 
 
