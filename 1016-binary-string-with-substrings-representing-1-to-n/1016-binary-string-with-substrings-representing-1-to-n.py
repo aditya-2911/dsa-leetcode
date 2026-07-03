88ab16625f808ecee9662cb5fba147539d61301f
Aditya Arun Ghorpade <118067181+aditya-2911@users.noreply.github.com>
2026-07-03 16:18:26 +0530

    Time: 3 ms (29.56%), Space: 19.3 MB (80.29%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.py
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.py
@@ -4,13 +4,15 @@ class Solution:
         if n > 2000:
             return False
         freq=set()
-        for i in range(len(s)):
-            if s[i]=='0':
+        b=s.encode('ascii')
+
+        for i in range(len(b)):
+            if b[i]==48:
                 continue
             num=0
 
-            for j in range(i,min(i+30, len(s))):
-                num= (num<<1)|int(s[j])
+            for j in range(i,min(i+30, len(b))):
+                num= (num<<1)| (b[j] & 1)
                 if 1<=num<=n:
                     freq.add(num)
                     if len(freq)==n: return True
