e75dd59c6107228d6695d9191a0922b79e2efad5
Aditya Arun Ghorpade <118067181+aditya-2911@users.noreply.github.com>
2026-07-03 18:23:59 +0530

    Update 0076-minimum-window-substring.py

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,44 +1,38 @@
 class Solution:
+    def func(self,freq_s,freq_t)->bool:
+        for key,value in freq_t.items():
+            if key in freq_s and freq_s[key]>=value:
+                continue
+            else:
+                return False
+        return True
     def minWindow(self, s: str, t: str) -> str:
-        freq_t = [0] * 128
-        required = 0
-        
+        freq_t=dict()
         for i in t:
-            idx = ord(i)
-            if freq_t[idx] == 0:
-                required += 1
-            freq_t[idx] += 1
-        filtered_s = []
-        for i in range(len(s)):
-            if freq_t[ord(s[i])] > 0:
-                filtered_s.append((i, ord(s[i])))
-
-        freq_s = [0] * 128
-        has = 0
+            if i in freq_t:
+                freq_t[i]+=1
+            else:
+                freq_t[i]=1
         
-        low = 0
-        start = 0
-        minLen = float('inf')
-
-        for high in range(len(filtered_s)):
-            curr_idx, char_code = filtered_s[high]
-            freq_s[char_code] += 1
+        freq_s={}
+        low=0
+        start=0
+        minLen=float('inf')
+        for high in range(len(s)):
+            if s[high] in freq_s:
+                freq_s[s[high]]+=1
+            else:
+                freq_s[s[high]]=1
             
-            if freq_s[char_code] == freq_t[char_code]:
-                has += 1
+            while self.func(freq_s,freq_t):
+                length=high-low+1
 
-            while has == required:
-                left_idx, left_code = filtered_s[low]
+                if length<minLen:
+                    minLen=length
+                    start=low
                 
-                length = curr_idx - left_idx + 1
-                if length < minLen:
-                    minLen = length
-                    start = left_idx
-                freq_s[left_code] -= 1
-                
-                if freq_s[left_code] < freq_t[left_code]:
-                    has -= 1
-                    
-                low += 1
-                
+                freq_s[s[low]]-=1
+                if freq_s[s[low]]==0:
+                    del freq_s[s[low]]
+                low+=1
         return s[start:start+minLen] if minLen != float('inf') else ""
