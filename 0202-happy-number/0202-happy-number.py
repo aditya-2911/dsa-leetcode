e5d9a944af7ef0e341728e0761a595e2174d940d
Aditya Arun Ghorpade <118067181+aditya-2911@users.noreply.github.com>
2026-07-05 13:31:19 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.05%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,15 +1,10 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
 
-        def digitSquare(num):
-            return sum(int(char) ** 2 for char in str(num))
-
-        slow = n
-        fast = digitSquare(n)
-
-        while fast != 1 and slow != fast:
-            slow = digitSquare(slow)
-            fast = digitSquare(fast)
-            fast = digitSquare(fast)
-
-        return fast == 1
+        while n != 1 and n != 4:
+            s = 0
+            while n > 0:
+                s += (n % 10) ** 2
+                n //= 10
+            n = s
+        return n == 1
