82571bf9a10a939938848d7874f365edf5251fe7
Aditya Arun Ghorpade <118067181+aditya-2911@users.noreply.github.com>
2026-09-15 20:28:07 +0530

    Time: 2628 ms (15.98%), Space: 50.5 MB (34.02%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
+        n = len(s)
+        is_pal = [[False] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (
+                    j - i <= 2 or is_pal[i + 1][j - 1]
+                ):
+                    is_pal[i][j] = True
+
+        dp = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            dp[i] = dp[i - 1]
+
+            for j in range(i):
+                if i - j >= k and is_pal[j][i - 1]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return dp[n]
